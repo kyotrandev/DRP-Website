d4b84cf7300ac5a4e8e2f09e88d7ac671e1e83f9
John Naeder <johnnaeder6537@gmail.com>
2024-03-24 06:41:07 +0700

    Fix ingredient manager page UI

--- Web/App/Views/admin/ingredient.php
+++ Web/App/Views/admin/ingredient.php
@@ -12,7 +12,7 @@
                                 border: 1px solid #e1ebfa; border-radius: 10px; 
                                 box-shadow: 0 0 10px 0 #e1ebfa; margin-top: 40px; margin-bottom: 50px;">
             <h4 class="mb-3" style="width: auto;">
-                <span>List user</span>
+                <span>List ingredient</span>
             </h4>
 
             <div class="row g-2">
@@ -25,7 +25,7 @@
                             <input type="text" class="form-control" id="category" name="category" placeholder="Category...">
                         </div>
                         <div class="col-2">
-                            <input type="text" class="form-control" id="measurement_desciption" name="measurement_desciption" placeholder="Measurement Desciption...">
+                            <input type="text" class="form-control" id="measurement_desciption" name="measurement_desciption" placeholder="Measurement unit...">
                         </div>
                         <div class="col-3">
                             <input type="text" class="form-control" id="name" name="name" placeholder="Name...">
@@ -34,7 +34,7 @@
                             <button class="btn btn-success" name="search" type="submit">Search</button>
                         </div>
                         <div class="col-auto"> 
-                            <a href="/ingredient/add" class="btn btn-success" tabindex="-1" role="button">Add new recipe</a>                        
+                            <a href="/ingredient/add" class="btn btn-success" tabindex="-1" role="button">Add new ingredient</a>                        
                         </div>
                     </form>
                 </div>
@@ -42,83 +42,90 @@
 
             <!-- TABLE -->
             <div class="row g-0 py-1">
-                <table class="table table-bordered nav">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Measurement Desciption</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                    <?php $count = 0; 
-                    if(!is_array($ingredients)){
-                        $ingredients = [$ingredients];
-                    }
-                    foreach ($ingredients as $ingredient): 
-                        $count++;
-                        if ($count > 10):
-                            break;
-                        endif;?>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
                         <tr>
-                            <td><?= $ingredient->getId()?></td>
-                            <td><?= $ingredient->getCategory()?></td>
-                            <td><?= $ingredient->getMeasurementUnit()?></td>
-                            <td><?= $ingredient->getName()?></td>
-                            <td>
-                                <form  class="set-active-form d-inline-block" method="POST">
-                                    <input type="hidden" name="id" value="<?= $ingredient->getId() ?>">
-                                    <input type="hidden" name="isActive" value="<?= $ingredient->getActive() ^ 1?>">
-                                    <button class="btn <?=$ingredient->getActive() ? 'btn-danger' : 'btn-success' ?>" style="width: 150px" type="submit">
-                                        <?= $ingredient->getActive() ? 'Deactivate' : 'Activate' ?>
-                                    </button>
-                                </form>
-                                <a href="/manager/ingredient/update?id=<?= $ingredient->getId() ?>" class="btn btn-secondary d-inline-block" role="button">Edit</a>
-                            </td>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Measurement unit</th>
+                            <th>Actions</th>
                         </tr>
-                    <?php endforeach; ?>
-                </table>
+                        <?php $count = 0; 
+                        if(!is_array($ingredients)){
+                            $ingredients = [$ingredients];
+                        }
+                        foreach ($ingredients as $ingredient): 
+                            $count++;
+                            if ($count > 10):
+                                break;
+                            endif;?>
+                            <tr>
+                                <td><?= $ingredient->getId()?></td>
+                                <td><?= $ingredient->getName()?></td>
+                                <td><?= $ingredient->getCategory()?></td>
+                                <td><?= $ingredient->getMeasurementUnit()?></td>
+                                <td style="display: flex; justify-content: space-between;">
+                                    <div>
+                                        <form  class="set-active-form d-inline-block" method="POST">
+                                            <input type="hidden" name="id" value="<?= $ingredient->getId() ?>">
+                                            <input type="hidden" name="isActive" value="<?= $ingredient->getActive() ^ 1?>">
+                                            <button class="btn <?=$ingredient->getActive() ? 'btn-danger' : 'btn-success' ?>" style="width: 150px" type="submit">
+                                                <?= $ingredient->getActive() ? 'Deactivate' : 'Activate' ?>
+                                            </button>
+                                        </form>
+                                    </div>
+                                    <div>
+                                        <a href="/manager/ingredient/update?id=<?= $ingredient->getId() ?>" class="btn btn-secondary d-inline-block" role="button">Edit</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+
             </div>
+
+ 
         </div>
     </div>
-
-    <div class="d-md-flex justify-content-center py-3">
-        <a href="/ingredient/add" class="btn btn-success" tabindex="-1" role="button">Add ingredient</a>
-    </div>
-    <div class="d-md-flex justify-content-center py-3">
-        <a href="/manager" class="btn btn-secondary me-md-2" tabindex="-1" role="button">Back</a>
+    <div class="d-md-flex justify-content-center py-3" >
+        <a href="/manager" class="btn btn-secondary me-md-2" tabindex="-1" role="button" style="width: 10%;">Back</a>
     </div>
 </div>
 
 <script src="/Public/js/validate-signup.js"></script>
 
 <script>
-  $(document).ready(function() {
-    $('.set-active-form').submit(function(event) {
-      event.preventDefault();
+$(document).ready(function() {
+  $('.set-active-form').submit(function(event) {
+    event.preventDefault();
 
-      var formData = $(this).serialize();
+    var formData = $(this).serialize();
+    var url = $(this).attr('action');
 
-      var url = $(this).attr('action');
+    var button = $(this).find('button[type="submit"]'); // Get the button element
 
-      $.ajax({
-        type: 'POST',
-        url: url,
-        data: formData,
-        dataType: 'json',
-        success: function(response) {
-          if (response.success) {
-            alert(response.message);
-            location.reload(); 
-          } else {
-            alert(response.message);
-          }
-        },
-        error: function(xhr, status, error) {
-          console.error(error);
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      success: function(response) {
+        if (response.success) {
+          alert(response.message);
+          // Toggle button class and text
+          button.toggleClass('btn-danger btn-success').text(response.isActive ? 'Deactivate' : 'Activate');
+        } else {
+          alert(response.message);
         }
-      });
+      },
+      error: function(xhr, status, error) {
+        console.error(error);
+      }
     });
   });
+});
 </script>
 
 
